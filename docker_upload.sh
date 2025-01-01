28442c81dffd321861de26c71e5e04f29ca4b9c8
han-a-leum <hal0576@kotech.co.kr>
2025-01-02 08:07:38 +0900

    Update docker_upload.sh

--- docker_upload.sh
+++ docker_upload.sh
@@ -3,16 +3,15 @@
 unzip example_code_v01.zip -d sample_code
 
 cd sample_code
-
 docker build -t example .
 
 cd -
 
 docker run -it -d \
--h example_docker \
---gpus all \
---shm-size 192g \
--v ./data:/shared/data \
--v ./models:/shared/models \
---name example_docker \
-example bash
+	-h example_docker \
+	--gpus all \
+	--shm-size 192g \
+	-v $PWD/data:/shared/data \
+	-v $PWD/models:/shared/models \
+	--name example_docker \
+	example /bin/bash
